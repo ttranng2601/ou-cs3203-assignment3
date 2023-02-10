a772455297291519f2e55127b8ad28c35d71c678
Thi Thuy Trang Tran <Thi.Thuy.Trang.Tran-1@ou.edu>
2023-02-10 10:42:25 -0600

    Add a main function

--- assignment.py
+++ assignment.py
@@ -9,3 +9,13 @@ def multiplication (num_list):
     for i in num_list:
         result *= i
     return result
+
+def main ():
+    input_list = list(input("Enter a list of numbers, separate each number by space: ").split())
+    num_list = [int(i) for i in input_list]
+    print("Sum of the list: " + str(sum(num_list)))
+    print("Product of the list: " + str(multiplication(num_list)))
+    
+if __name__ == '__main__':
+    main()
+    
