cdbab3b3db7e77e59f2d931d0bb8cfbfdbb58d1e
Thi Thuy Trang Tran <Thi.Thuy.Trang.Tran-1@ou.edu>
2023-02-10 10:56:16 -0600

    Add some comments to file

--- assignment.py
+++ assignment.py
@@ -1,15 +1,18 @@
+#Question 1: A sum function
 def sum (num_list):
     sum = 0
     for i in num_list:
         sum +=i
     return sum
 
+#Question 2: A multiplication function
 def multiplication (num_list):
     result = 1
     for i in num_list:
         result *= i
     return result
 
+#Question 3: Add a main function
 def main ():
     input_list = list(input("Enter a list of numbers, separate each number by space: ").split())
     num_list = [int(i) for i in input_list]
