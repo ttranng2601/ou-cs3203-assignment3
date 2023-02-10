4ca8ed71c6001f8b11e4564752d3757d62be59f9
Thi Thuy Trang Tran <Thi.Thuy.Trang.Tran-1@ou.edu>
2023-02-10 10:43:58 -0600

    Add a reverse function to main

--- assignment.py
+++ assignment.py
@@ -10,15 +10,18 @@ def multiplication (num_list):
         result *= i
     return result
 
+def reverse (num_list):
+    r_list = num_list[::-1]
+    return r_list 
+
 def main ():
     input_list = list(input("Enter a list of numbers, separate each number by space: ").split())
     num_list = [int(i) for i in input_list]
     print("Sum of the list: " + str(sum(num_list)))
     print("Product of the list: " + str(multiplication(num_list)))
+    print("The reversed list: " + str(reverse(num_list)))
     
 if __name__ == '__main__':
     main()
     
-def reverse (num_list):
-    r_list = num_list[::-1]
-    return r_list     
+    
